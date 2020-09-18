41767d77ad9d53aca7783f28b205e93282e7d1a4
leandrocarra <leandrocarra@gmail.com>
2020-09-18 01:49:10 -0300

    script deploy

---
+++ build/precache-manifest.e7993c657e83775563ce4e2545994ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d143eb45e1ce56fedd56bcf3e26749",
+    "url": "/reporepo/index.html"
+  },
+  {
+    "revision": "f0d3c45046eb2df0b71c",
+    "url": "/reporepo/static/js/2.6f5bbac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reporepo/static/js/2.6f5bbac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297ba09671e390653331",
+    "url": "/reporepo/static/js/main.795b6663.chunk.js"
+  },
+  {
+    "revision": "136d774d53cd3bfd59e1",
+    "url": "/reporepo/static/js/runtime-main.0850456c.js"
+  },
+  {
+    "revision": "2532bbceb19f613918801063e34ce7c3",
+    "url": "/reporepo/static/media/fight.2532bbce.gif"
+  }
+]);
